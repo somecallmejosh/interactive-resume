40c6bbdc9d0b72abf80dd0540dfccc63be101754
Joshua Briley <josh@thebrileys.com>
2014-12-27 22:16:07 -0500

    Update work data.

--- js/json/work.js
+++ js/json/work.js
@@ -16,7 +16,7 @@ var work = {
       "title" : "Front End Developer",
       "location" : "East Hartford, CT",
       "dates" : "Jan 13 - May 13",
-      "description" : "<p>Front End Design and Development of Online Housing Reposession Industry Network.</p><p>Lead UX/UI designer on short term, large scale web application.</p><p>Planned and developed landing page testing scenarios for maximum conversion.</p><p>Saved company $10K/year by minimizing server side dependencies on company website.</p>"
+      "description" : "<p>Front End Design & Development of social business application.</p><p>Lead UX/UI designer on short term, large scale web application.</p><p>Saved company $10K/year by minimizing server side dependencies on company website.</p>"
     },
     {
       "employer" : "ImageWorks, LLC",
@@ -25,7 +25,7 @@ var work = {
       "title" : "Lead Designer",
       "location" : "Vernon, CT",
       "dates" : "Aug 08 - Jan 13",
-      "description" : "<p>Front End Design and Development of conversion oriented marketing websites.</p><p>Strategized and implemented systems that decreased design and front end development time by more than 50%.</p><p>Implemented \"Lunch-and-Learn Friday\" to train staff on standards based front end development techniques.</p><p>Designed and developed projects featured on The Food Network and the Hartford Business Journal.</p>"
+      "description" : "<p>Front End Design & Development of marketing websites.</p><p>Decreased organizational project development time by more than 50%.</p><p>Designed and developed projects featured on The Food Network and the Hartford Business Journal.</p>"
     },
     {
       "employer" : "Para-Diddle Design, LLC",
@@ -34,7 +34,7 @@ var work = {
       "title" : "Co-Owner",
       "location" : "56 Starkweather St., Manchester, CT USA",
       "dates" : "Jan 04 - Aug 08",
-      "description" : "<p>Front End Design and Development of conversion oriented marketing websites.</p><p>Responsible for vision, budgeting, marketing/sales and other administrative tasks.</p><p>Educated clients on best practices and conversion oriented design principles.</p><p>Increased productivity by training staff on efficient coding and workflow practices.</p>"
+      "description" : "<p>Front End Design & Development of small business websites.</p><p>Responsible for vision, budgeting, marketing/sales and other administrative tasks.</p><p>Increased team productivy through consistent training.</p>"
     }
   ],
   "display" : function() {
